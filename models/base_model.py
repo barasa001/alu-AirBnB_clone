9765cde0bdf268a1a513f95a7378fbc8ca27e682
d3ng4 <denggi2000@gmail.com>
2023-01-28 12:21:09 -0800

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -19,14 +19,15 @@ class models:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self.to_dict())
 
         def __str__(self):
             """return string representaion of the class"""
-            return "[{}] ({}) {}".format(self.__class__.__name
+            return "[{}] ({}) {}".format(__class__.__name__, self.id.__dict__)
 
          def save(self):
-        """ update of attribute updated_at with latest time
+        """ 
+        update of attribute updated_at with latest time
         updated_at: datetime
         """
         self.updated_at = datetime.now()
@@ -34,8 +35,7 @@ class models:
 
     def to_dict(self):
         """
-        get the objects of the instance
-        returns: dictionary
+        Returns a dictionary containing all keys/values of __dict__ of the instance
         """
         temp_dict = dict(self.__dict__)
         temp_dict['__class__'] = __class__.__name__
