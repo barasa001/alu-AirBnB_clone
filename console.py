eb903493aa06a9c70dfa45efa55ab69f0278fef8
barasa001 <s.wanyonyi@alustudent.com>
2023-01-29 23:31:36 -0800

    The file

--- console.py
+++ console.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
 """Command line interpreter"""
-import cmd 
+import cmd
+import sys
+
+class HBNBCommand(cmd.Cmd):
+    """for the prompt"""
+
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """For the end of the file"""
+        return True
+
+    def do_quit(self, file):
+        """To exit the program"""
+        sys.exit()
+
+    def emptyline(self):
+        """for overwriting"""
+        print('', end="")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
