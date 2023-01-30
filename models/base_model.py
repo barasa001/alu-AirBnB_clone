2bc002a20bb86fd4ed74ca700f7c1a27888f9d06
d3ng4 <denggi2000@gmail.com>
2023-01-30 11:39:16 -0800

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,50 @@
-#!/bin/usr/python3
-"""defines base model class"""
+#!/usr/bin/python3
+""" this is the base class for all models """
 import uuid
-from datetime import datetime
-import models
+from datetime import date, datetime, time
+from . import storage
 
 
 class BaseModel:
-    """Base model class"""
-    def __init(self, args, **kwargs):
-        """initiate class"""
+    """class defining commom attributes
+    for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """ instance attributes
+        id : string
+        created_at: datetime
+        updated_at: datetime
+        getting arguments to recreate BaseModel
+        creates new instance Model
+        """
+        # self.id = str(uuid.uuid4())
+        # self.created_at = datetime.now()
+        # self.updated_at = self.created_at
+
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key !="__class__":
-                    setattr(self, key, value)
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        temp_dict = self.__dict__
+                        temp_dict[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        temp_dict = self.__dict__
+                        temp_dict[key] = value
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self.to_dict())
 
-        def __str__(self):
-            """return string representaion of the class"""
-            return "[{}] ({}) {}".format(__class__.__name__, self.id.__dict__)
+    def __str__(self):
+        """ default string output of class name \
+                ,id and dictionary files """
+        return '[{}] ({}) <{}>'\
+            .format(__class__.__name__, self.id, self.__dict__)
 
-         def save(self):
-        """ 
-        update of attribute updated_at with latest time
+    def save(self):
+        """ update of attribute updated_at with latest time
         updated_at: datetime
         """
         self.updated_at = datetime.now()
@@ -35,11 +52,11 @@ class BaseModel:
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance
+        get the objects of the instance
+        returns: dictionary
         """
         temp_dict = dict(self.__dict__)
         temp_dict['__class__'] = __class__.__name__
         temp_dict['created_at'] = temp_dict['created_at'].isoformat()
         temp_dict['updated_at'] = temp_dict['updated_at'].isoformat()
         return temp_dict
-
