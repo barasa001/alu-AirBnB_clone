e64b5c7c3c024606f6e2d7505520131d6a667ab4
d3ng4 <denggi2000@gmail.com>
2023-01-30 11:43:14 -0800

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" creating unique instance for application """
+""" __init__.py method for model package """
 from .engine import file_storage
 
 storage = file_storage.FileStorage()
