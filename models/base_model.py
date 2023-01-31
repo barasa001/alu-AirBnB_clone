4c26525fd53447e7672899cb669e1917f75b93ec
barasa001 <s.wanyonyi@alustudent.com>
2023-01-31 06:12:21 -0800

    Working on the attributes again

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,19 @@ from datetime import datetime
 
 class BaseModel:
     """contains common attribute"""
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.now()
-    self.updated_at = datetime.now()
+    def __init__self(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
