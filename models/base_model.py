5a189e33d34c2bcab7f70896af3588b75b6a62e2
barasa001 <s.wanyonyi@alustudent.com>
2023-01-30 23:13:25 -0800

    Strated basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,37 +2,11 @@
 """Our BaseModel that defines all common attributes/methods for other classes
 """
 
+import uuid
 from datetime import datetime
-import models
-
 
 class BaseModel:
-    """The main class"""
-    def __init__(self, *args, **kwargs):
-        """Constructor of the instance. Uses kwargs if not empty"""
-        self.id = str(uuid.uuid4())
-        self.updated_at = datetime.now()
-        self.created_at = datetime.now()
-
-    def save(self):
-        """update time attribute"""
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        """return string representation"""
-        class_name = "[" + self.__class__.__name__ + "]"
-        return class_name + " (" + self.id + ") " + str(self.__dict__)
-
-    def to_dict(self):
-        """return dict containing keys/values"""
-        new_dict = {}
-
-        for key, values in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                new_dict[key] = values
-       new_dict['__class__'] = self.__class__.__name__
-
-       return new_dict
-
+    """contains common attribute"""
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
