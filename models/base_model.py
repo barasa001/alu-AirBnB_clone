3936e346f70a74efc3bf1b773196e14754f62518
barasa001 <s.wanyonyi@alustudent.com>
2023-01-30 23:08:05 -0800

    The commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Our BaseModel that defines all common attributes/methods for other classes
 """
-import uuid
+
 from datetime import datetime
 import models
 
 
-class BaseModel():
+class BaseModel:
     """The main class"""
     def __init__(self, *args, **kwargs):
         """Constructor of the instance. Uses kwargs if not empty"""
