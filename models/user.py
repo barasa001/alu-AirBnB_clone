69ccd9f9228a17f38744c66e66988983b424edc2
barasa001 <s.wanyonyi@alustudent.com>
2023-02-01 05:16:07 -0800

    Made changes

--- models/user.py
+++ models/user.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""for the user class"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
