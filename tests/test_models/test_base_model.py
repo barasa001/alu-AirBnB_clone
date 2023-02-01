b968c4d6e7a0de835ad277f8e4eb4438926ce423
barasa001 <s.wanyonyi@alustudent.com>
2023-01-31 20:56:08 -0800

    test file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""unittest for base model class"""
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
+from datetime import datetime
+from unittest import TestCase
+import uuid
+if __name__ == '__main__':
+    unittest.main()
