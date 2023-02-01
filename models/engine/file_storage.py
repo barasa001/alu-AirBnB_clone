89eba151a822f7c595ba33fe20806cfc0e36f640
barasa001 <s.wanyonyi@alustudent.com>
2023-02-01 06:20:36 -0800

    edit filestorage attr name

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,7 +12,7 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        key = obj.__class__.name__ + "." + obj.id
+        key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
