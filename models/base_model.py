346e95dc750d3c4e33cc73eab66bbcc0a634abc6
barasa001 <s.wanyonyi@alustudent.com>
2023-02-01 04:58:42 -0800

    The changes on base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,18 +4,32 @@
 
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
     """contains common attribute"""
-    def __init__self(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+    def __init__self(self, *arg, **kwargs):
+        if not kwargs:
+            
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            f= "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        class_name = "[" + self.__class__.__name__ + "]"
+        lst = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+                return class_name + " (" + self.id + ") " + str(lst)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         data = self.__dict__.copy()
