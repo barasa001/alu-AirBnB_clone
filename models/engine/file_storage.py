8752258e07297ee6aaf089680064ad712775c25c
barasa001 <s.wanyonyi@alustudent.com>
2023-02-01 04:20:12 -0800

    Filestorage edits

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,12 +16,24 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
+        dictionary = {}
+
+        for key, value in Filestorage.__objects.items():
+            dictionary[key] = value.to_dict()
         with open(FileStorage.__file_path, 'w', encoding= 'utf-8') as f:
-            json.dump({key: obj.to_dict() for key, obj in FileStorage.__objects.items()}, f)
+            json.dump(dictionary, f)
 
     def reload(self):
-        try:
-            with open (FileStorage.__file_path, 'r', encoding='utf-8') as f:
-                FileStorage.__objects = {key: obj.__class__(**value) for key, value in json.load(f).items()}
-        except FileNotFoundError:
-            pass
+        from models.user import User
+        from models.review import Review
+        from models.amenity import Amenity
+        from models.base_model import BaseModel
+        from models.city import City
+        from models.place import Place
+        from models.state import State
+        lst = {'BaseModel': BaseModel, 'User': User, 'Place': Place, 'City': City, 'Amenity': Amenity, 'State': State, 'Review': Review}
+
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.new(lst[value['__class__']](**value))
