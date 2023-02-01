0dac8c0b15b5e11f7de8f528f74deea8496c140c
barasa001 <s.wanyonyi@alustudent.com>
2023-02-01 05:06:16 -0800

    The commit on place

--- models/place.py
+++ models/place.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Attributes for the place class"""
+
+class Place(BaseModel):
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_bathrooms = 0
+    number_rooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
