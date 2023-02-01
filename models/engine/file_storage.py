b0b267b62f906d562f9cc286bd9da2c43d4ed688
barasa001 <s.wanyonyi@alustudent.com>
2023-01-31 20:31:32 -0800

    Made changes to filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,5 +4,24 @@ import os.path
 import json
 
 
-class FileStorage():
-    pass
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = obj.__class__.name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        with open(FileStorage.__file_path, 'w', encoding= 'utf-8') as f:
+            json.dump({key: obj.to_dict() for key, obj in FileStorage.__objects.items()}, f)
+
+    def reload(self):
+        try:
+            with open (FileStorage.__file_path, 'r', encoding='utf-8') as f:
+                FileStorage.__objects = {key: obj.__class__(**value) for key, value in json.load(f).items()}
+        except FileNotFoundError:
+            pass
