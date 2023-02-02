c510038a1708cef0ec1db4cd0db9610dd2d7a333
barasa001 <s.wanyonyi@alustudent.com>
2023-02-01 23:23:56 -0800

    removed space

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
         self.updated_at = datetime.now()
         f = "%Y-%m-%dT%H:%M:%S.%f"
         if not kwargs:
-            
+
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             storage.new(self)
