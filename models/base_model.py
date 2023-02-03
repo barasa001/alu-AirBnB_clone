35849d848dc1ff018542dc57eb65d336b5d24c11
barasa001 <s.wanyonyi@alustudent.com>
2023-02-02 23:42:54 -0800

    made changes on basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -10,22 +10,19 @@ import models
 class BaseModel:
     """contains common attribute"""
     def __init__self(self, *arg, **kwargs):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        f = "%Y-%m-%dT%H:%M:%S.%f"
-        if not kwargs:
-
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            f= "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs.__len__() > 0:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], f)
+                    value = datetime.fromisoformat(v)
+                    setattr(self, key, value)
+                    continue
                 if key != '__class__':
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         class_name = "[" + self.__class__.__name__ + "]"
