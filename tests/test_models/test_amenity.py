9601d105b53e30f795a9ad1710ac5f658cdf099f
barasa001 <s.wanyonyi@alustudent.com>
2023-02-06 04:41:42 -0800

    the commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""Unittest for the amenity class"""
+
+import unittest
+from models.amenity import Amenity
+import datetime
+
+
+class testAmenity(unittest.Testcase):
+    """Methods and instances test"""
+
+    amen = Amenity()
+
+    def test_class(self):
+        """check class"""
+        abs = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.amen)), abs)
+
+    def test_inheritance(self):
+        """check if it inherited"""
+        self.assertIsInstance(self.a, Amenity)
+
+    def test_attrs(self):
+        """check the attributes"""
+        self.assertTrue(hasattr(self.amen, 'id'))
+        self.assertTrue(hasattr(self.amen, 'created_at'))
+        self.assertTrue(hasattr(self.amen, 'name'))
+        self.assertTrue(hasattr(self.amen, 'udated_at'))
+
+    def test_type(self):
+        """check the attribute type"""
+        self.assertIsInstance(self.amen.id, str)
+        self.assertIsInstance(self.amen.name, str)
+        self.assertIsInstance(self.amen.updated_at, datetime.datetime)
+        self.assertIsInstance(self.amen.created_at, datetime.datetime)
+
+if __name__ == '__main__':
+    unittest.main()
