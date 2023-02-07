3bd7805648f709e8ede0d03a355425836fd471b5
barasa001 <s.wanyonyi@alustudent.com>
2023-02-07 01:00:03 -0800

    check

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -74,7 +74,7 @@ class TestFStorage(unittest.TestCase):
         self.assertNotEqual(dobj, FileStorage._FileStorage__objects)
         storage.reload()
         for key, value in storage.all().items():
-            self.assertDictEqual(dobj[key].to_dict(), value.to_dict())
+            self.assertGreater(dobj[key].to_dict(), value.to_dict())
 
     def testSaveSelf(self):
         """checking self"""
